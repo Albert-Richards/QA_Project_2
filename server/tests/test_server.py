92df3a32cf930b86c8c8ea18cede8af67c05eb60
Albert <albert.richards3@gmail.com>
2021-07-15 13:35:26 +0000

    modified server test and Jenkinsfile

--- server/tests/test_server.py
+++ server/tests/test_server.py
@@ -1,12 +1,24 @@
 from flask import url_for
 from flask_testing import TestCase
 import requests_mock
-
-from app import app
+from app import app, db
 
 class TestBase(TestCase):
     def create_app(self):
+        app.config.update(SQLALCHEMY_DATABASE_URI="sqlite:///",
+                SECRET_KEY='TEST_SECRET_KEY',
+                DEBUG=True,
+                WTF_CSRF_ENABLED=False
+                )
         return app
+    
+    def setUp(self):
+        db.create_all()
+    
+    def tearDown(self):
+        
+        db.session.remove()
+        db.drop_all()
 
 class TestResponse(TestBase):
     def test_home(self):
