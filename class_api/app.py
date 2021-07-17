20fda26681e3ec2bed7a3d633f358d5c07b2ad8c
Albert <albertrichards3@gmail.com>
2021-07-17 12:29:00 +0000

    new deal

--- class_api/app.py
+++ class_api/app.py
@@ -1,3 +1,4 @@
+  
 from flask import Flask, request
 import random
 
@@ -5,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/get_class', methods=['GET'])
 def get_class():
-    return random.choice(['Nemesis', 'Bastion', 'Commando', 'Shock Trooper', 'Operative', 'Medic'])
+    return random.choice(['Adept', 'Engineer', 'Soldier', 'Vanguard', 'Sentinel', 'Infiltrator'])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
